99bbc45df727d6fdd3b5827ac17a6efb8769300e
ZhangTerrence <terrencezhang77@gmail.com>
2025-01-07 13:13:24 -0500

    feat: successfully retrieving data

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,13 @@ from os import environ
 
 import requests
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 app.config["API_KEY"] = environ.get("API_KEY")
+
+CORS(app, origins="*")
 
 @app.route('/')
 def index():
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useApi } from "@/hooks/useApi.tsx";
 
-function App() {
-  const [count, setCount] = useState(0);
+export type Data = {
+  date: string;
+  revenue: number;
+  netIncome: number;
+  grossProfit: number;
+  eps: number;
+  operatingIncome: number;
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
-}
+export const App = () => {
+  const { data, isLoading, error } = useApi<Data>("GET", "", null);
 
-export default App;
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (!data) {
+    return <div>Unable to retrieve data.</div>;
+  }
+
+  console.log(data);
+
+  return <div>Hello, world.</div>;
+};

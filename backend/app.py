d4b2bfbb403e302c03cf0dd6d582b46ac384acaf
ZhangTerrence <terrencezhang77@gmail.com>
2025-01-07 19:13:37 -0500

    feat: added redis caching

--- backend/app.py
+++ backend/app.py
@@ -3,18 +3,24 @@ from os import environ
 import requests
 from flask import Flask, request
 from flask_cors import CORS
+from flask_caching import Cache
 
 app = Flask(__name__)
-
-app.config["API_KEY"] = environ.get("API_KEY")
+cache = Cache(app, config={
+    "CACHE_TYPE": "RedisCache",
+    "CACHE_REDIS_HOST": environ.get("REDIS_HOST"),
+    "CACHE_REDIS_PORT": environ.get("REDIS_PORT"),
+    "CACHE_REDIS_DB": environ.get("REDIS_DB")
+})
 
 CORS(app, origins="*")
 
 
 @app.route("/")
+@cache.cached(timeout=100, query_string=True)
 def index():
     response = requests.get(
-        f"https://financialmodelingprep.com/api/v3/income-statement/AAPL?period=annual&apikey={app.config["API_KEY"]}")
+        f"https://financialmodelingprep.com/api/v3/income-statement/AAPL?period=annual&apikey={environ.get("API_KEY")}")
     data = response.json()
 
     column = request.args.get("column")
@@ -23,8 +29,9 @@ def index():
     argmin = request.args.get("min")
     argmax = request.args.get("max")
     if column and argmin and argmax:
-        data = list(filter(lambda x: int(argmin) <= (int(x["calendarYear"]) if column == "date" else x[column]) <= int(argmax),
-                      list(data)))
+        data = list(
+            filter(lambda x: int(argmin) <= (int(x["calendarYear"]) if column == "date" else x[column]) <= int(argmax),
+                   list(data)))
 
     # Sort
     asc = request.args.get("asc")
